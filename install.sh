cfb0935db3a0323d31d7ac8087ec0b2126173a0e
Pixelcraft Innovations <ap@pixelcraft.ro>
2024-12-19 01:50:17 +0200

    Update install.sh
    + added pacs service

--- install.sh
+++ install.sh
@@ -1,32 +1,44 @@
-#!/bin/bash
-
-# Set Variables
-STACKS_DIR="/opt/stacks"
-DOCKER_COMPOSE_URL="https://dockge.kuma.pet/compose.yaml?port=5001&stacksPath=%2Fopt%2Fstacks"
-
-echo "---- Starting DockGE Installation ----"
-
-# Ensure root privileges
-if [ "$EUID" -ne 0 ]; then
-  echo "Please run as root or use sudo."
-  exit 1
-fi
-
-# Update System
-echo "Updating packages..."
-apt-get update -y && apt-get upgrade -y
-
-# Create Stacks Directory
-echo "Creating directory structure..."
-mkdir -p "$STACKS_DIR"
-
-# Download Docker Compose File for DockGE
-echo "Downloading official Docker Compose file..."
-wget -O "$STACKS_DIR/docker-compose.yml" "$DOCKER_COMPOSE_URL"
-
-# Deploy Docker Stack
-echo "Deploying DockGE with Docker Compose..."
-cd "$STACKS_DIR" && docker compose up -d
-
-echo "---- DockGE Installation Completed ----"
-echo "Access DockGE at your server's IP address with port 5001."
+version: "3.8"
+services:
+  dockge:
+    image: louislam/dockge:1
+    restart: unless-stopped
+    ports:
+      - 5001:5001
+    volumes:
+      - /var/run/docker.sock:/var/run/docker.sock
+      - ./data:/app/data
+      # Stacks Directory
+      # ⚠️ READ IT CAREFULLY. If you did it wrong, your data could end up writing into a WRONG PATH.
+      # ⚠️ 1. FULL path only. No relative path (MUST)
+      # ⚠️ 2. Left Stacks Path === Right Stacks Path (MUST)
+      - /opt/stacks:/opt/stacks
+    environment:
+      # Tell Dockge where to find the stacks
+      - DOCKGE_STACKS_DIR=/opt/stacks
+  pacs:
+    image: orthancteam/orthanc
+    restart: unless-stopped
+    ports: ["443:443"]
+    volumes:
+      - orthanc-storage:/var/lib/orthanc/db/
+      - ./tls:/tls
+    environment:
+      DICOM_WEB_PLUGIN_ENABLED: "true"
+      OHIF_PLUGIN_ENABLED: "true"
+      ORTHANC_JSON: |
+        {
+          "Name" : "Orthanc for HTTPS tests",
+          "AuthenticationEnabled": false,
+          "OHIF": {
+            "DataSource": "dicom-web"
+          },
+          "RemoteAccessAllowed": true,
+          "SslEnabled": true,
+          "SslCertificate": "/tls/orthanc-https.pem",
+          "HttpPort": 443
+        }
+      VERBOSE_ENABLED: "true"
+      VERBOSE_STARTUP: "true"
+volumes:
+  orthanc-storage:
